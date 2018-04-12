e3a0b4e9e5fac1502f0830800a7acf9bcae9ae96
ggregor <38314605+ggregor@users.noreply.github.com>
2018-04-12 11:20:16 +0200

    Update submit.php

--- submit.php
+++ submit.php
@@ -7,7 +7,7 @@ if(isset($_GET['files']))
 	$error = false;
 	$files = array();
 
-	$uploaddir = './uploads/';
+	$uploaddir = 'uploads/';
 	foreach($_FILES as $file)
 	{
 		if(move_uploaded_file($file['tmp_name'], $uploaddir .basename($file['name'])))
